d1f50b78ffc70fc716a49ca79dd73517b50eea9e
Dom-Domz <49261474+Dom-Domz@users.noreply.github.com>
2019-04-05 09:09:59 +0800

    Add files via upload

--- metasploit.sh
+++ metasploit.sh
@@ -1,74 +1,68 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#! /data/data/com.termux/files/usr/bin/bash
 
-cwd=$(pwd)
-name=$(basename "$0")
-export msfinst="$cwd/$name"
-#sha_actual=$(sha256sum $(echo $msfinst))
-#echo $sha_actual
-if [ $name != "metasploit.sh" ]; then
-	echo "[-] Please do not use third-party stolen scripts"
-	exit 1
+# Hapus Folder Lama jika ada
+temukan $ HOME -nama " metasploit * " -type d -exec rm -rf {} \;
+
+
+cwd = $ ( pwd )
+msfvar = 4.17.40
+msfpath = ' /data/data/com.termux/files/home '
+
+pembaruan apt
+apt install -y autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev pg-dev buat ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
+# Banyak ponsel yang mengklaim libxml2 tidak ditemukan kesalahan
+ln -sf $ PREFIX / include / libxml2 / libxml $ PREFIX / include /
+
+cd  $ msfpath
+curl -LO https://github.com/rapid7/metasploit-framework/archive/ $ msfvar .tar.gz
+
+tar -xf $ msfpath / $ msfvar .tar.gz
+mv $ msfpath / metasploit-framework- $ msfvar  $ msfpath / metasploit-framework
+cd  $ msfpath / metasploit-framework
+sed ' / rbnacl / d ' -i Gemfile.lock
+sed ' / rbnacl / d ' -i metasploit-framework.gemspec
+permata instal bundler
+
+isNokogiri = $ ( daftar permata nokogiri -i )
+
+sed ' s | nokogiri (1. *) | nokogiri (1.8.0) | g ' -i Gemfile.lock
+
+if [ $ isNokogiri  ==  " false " ] ;
+kemudian
+      gem install nokogiri -v ' 1.8.0 ' - --use-system-libraries
+lain
+	echo  " nokogiri sudah terpasang "
 fi
 
-msfvar=4.17.37
-msfpath='/data/data/com.termux/files/home'
-if [ -d "$msfpath/metasploit-framework" ]; then
-	echo "deleting old version..."
-        rm $msfpath/metasploit-framework -rf
-fi
-apt update
-apt install -y autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
+cd  $ msfpath / metasploit-framework
+pembaruan permata --sistem
+bundel instal -j5
 
-cd $msfpath
-curl -LO https://github.com/rapid7/metasploit-framework/archive/$msfvar.tar.gz
-tar -xf $msfpath/$msfvar.tar.gz
-mv $msfpath/metasploit-framework-$msfvar $msfpath/metasploit-framework
-cd $msfpath/metasploit-framework
-if [ $(gem list -i rubygems-update) == false ]; then
-        gem install rubygems-update
-fi
+gema  " Permata terpasang "
+$ PREFIX / bin / find -type f -executable -exec termux-fix-shebang \ {\}  \;
+rm ./modules/auxiliary/gather/http_pdf_authors.rb
 
-update_rubygems
+# ln -sf $ msfpath / metasploit-framework / msfconsole /data/data/com.termux/files/usr/bin/
+# ln -sf $ msfpath / metasploit-framework / msfvenom /data/data/com.termux/files/usr/bin/
 
-gem install bundler
+termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.6.0/gems/pg-0.20.0/lib/pg_ext.so
+echo  " Membuat basis data "
 
-cd $msfpath/metasploit-framework
-bundle install -j5
+cd  $ msfpath / metasploit-framework / config
 
-echo "Gems installed"
-$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/database.yml
 
-if [ -e $PREFIX/bin/msfconsole ];then
-	rm $PREFIX/bin/msfconsole
-fi
-if [ -e $PREFIX/bin/msfvenom ];then
-	rm $PREFIX/bin/msfvenom
-fi
 
-echo "#!/data/data/com.termux/files/usr/bin/bash
-pg_ctl -D $PREFIX/var/lib/postgresql restart
-ruby $msfpath/metasploit-framework/msfconsole" > $PREFIX/bin/msfconsole
+mkdir -p $ PREFIX / var / lib / postgresql
+initdb $ PREFIX / var / lib / postgresql
 
-echo "#!/data/data/com.termux/files/usr/bin/bash
-ruby $msfpath/metasploit-framework/msfvenom" > $PREFIX/bin/msfvenom
+pg_ctl -D $ PREFIX / var / lib / postgresql mulai
+buat pengguna msf
+dibuatb msf_database
 
-chmod +rwx $PREFIX/bin/msfconsole
-chmod +rwx $PREFIX/bin/msfvenom
+rm $ msfpath / $ msfvar .tar.gz
 
-termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.4.0/gems/pg-0.20.0/lib/pg_ext.so
+cd  $ {PREFIX} / bin && curl -LO https://hax4us.github.io/files/msfconsole && chmod + x msfconsole
+ln -sf $ ( yang msfconsole )  $ PREFIX / bin / msfvenom
 
-echo "Creating database"
-
-cd $msfpath/metasploit-framework/config
-curl -LO https://Auxilus.github.io/database.yml
-
-mkdir -p $PREFIX/var/lib/postgresql
-initdb $PREFIX/var/lib/postgresql
-
-pg_ctl -D $PREFIX/var/lib/postgresql start
-createuser msf
-createdb msf_database
-
-rm $msfpath/$msfvar.tar.gz
-
-echo "you can directly use msfvenom or msfconsole rather than ./msfvenom or ./msfconsole as they are symlinked to $PREFIX/bin"
+gema  " Anda dapat langsung menggunakan msfvenom atau msfconsole daripada ./msfvenom atau ./
